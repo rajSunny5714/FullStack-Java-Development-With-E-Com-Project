c05d55e4f5da4ca1dcad1ea567d43744b1e51894
sunnyraj <mrsj5714@gmail.com>
2026-02-10 18:42:38 +0530

    files added

--- sb-ecom/src/main/java/com/ecommerce/project/controller/CategoryController.java
+++ sb-ecom/src/main/java/com/ecommerce/project/controller/CategoryController.java
@@ -1,7 +1,10 @@
 package com.ecommerce.project.controller;
 
 import com.ecommerce.project.model.Category;
+import com.ecommerce.project.service.CategoryService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -9,9 +12,19 @@ import java.util.List;
 
 @RestController
 public class CategoryController {
-    private List<Category> categories = new ArrayList<>();
+    private CategoryService categoryService;
+
+    public CategoryController(CategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
+
     @GetMapping("/api/public/categories")
     public List<Category> getAllCategories() {
-        return categories;
+        return categoryService.getAllCategories();
+    }
+    @PostMapping("/api/public/categories")
+    public String createCategory(@RequestBody Category category){
+        categoryService.createCategory(category);
+        return "Category added successfully";
     }
 }
