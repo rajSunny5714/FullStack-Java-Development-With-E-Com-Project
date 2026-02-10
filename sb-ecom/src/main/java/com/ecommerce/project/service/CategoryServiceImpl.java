bee166afd63666f598b74016e140866f41250ad4
sunnyraj <mrsj5714@gmail.com>
2026-02-10 19:16:33 +0530

    managing and deleting id's

--- sb-ecom/src/main/java/com/ecommerce/project/service/CategoryServiceImpl.java
+++ sb-ecom/src/main/java/com/ecommerce/project/service/CategoryServiceImpl.java
@@ -8,6 +8,7 @@ import java.util.List;
 @Service
 public class CategoryServiceImpl implements CategoryService {
     private List<Category> categories = new ArrayList<>();
+    private Long nextId=1L;
     @Override
     public List<Category> getAllCategories() {
         return categories;
@@ -15,6 +16,19 @@ public class CategoryServiceImpl implements CategoryService {
 
     @Override
     public void createCategory(Category category) {
+        category.setCategoryId(nextId++);
         categories.add(category);
+    }
+
+    @Override
+    public String deleteCategory(Long categoryId) {
+        Category category = categories.stream()
+                .filter(c-> c.getCategoryId().equals(categoryId))
+                .findFirst().orElse(null);
+
+        if(category==null)
+            return "Category not found.";
+        categories.remove(category);
+        return "Category with categoryId: "+categoryId+" deleted successfully.";
     }
 }
