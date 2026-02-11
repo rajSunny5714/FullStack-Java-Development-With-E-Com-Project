05bd0aae4aa78ec8653feebb4940f30d03121309
sunnyraj <mrsj5714@gmail.com>
2026-02-11 19:13:41 +0530

    understand request mapping

--- sb-ecom/src/main/java/com/ecommerce/project/controller/CategoryController.java
+++ sb-ecom/src/main/java/com/ecommerce/project/controller/CategoryController.java
@@ -9,7 +9,7 @@ import org.springframework.web.server.ResponseStatusException;
 
 import java.util.ArrayList;
 import java.util.List;
-
+@RequestMapping("/api")
 @RestController
 public class CategoryController {
     private CategoryService categoryService;
@@ -18,17 +18,19 @@ public class CategoryController {
         this.categoryService = categoryService;
     }
 
-    @GetMapping("/api/public/categories")
+    @GetMapping("/public/categories")
+//    @RequestMapping(value = "/public/categories", method = RequestMethod.GET)
     public ResponseEntity<List<Category>> getAllCategories() {
         List<Category> categories = categoryService.getAllCategories();
         return new ResponseEntity<>(categories, HttpStatus.OK);
     }
-    @PostMapping("/api/public/categories")
+    @PostMapping("/public/categories")
+//    @RequestMapping(value = "/public/categories", method = RequestMethod.POST)
     public ResponseEntity<String> createCategory(@RequestBody Category category){
         categoryService.createCategory(category);
         return new ResponseEntity<>("Category added successfully", HttpStatus.CREATED);
     }
-    @DeleteMapping("/api/admin/categories/{categoryId}")
+    @DeleteMapping("/admin/categories/{categoryId}")
     public ResponseEntity<String> deleteCategory(@PathVariable Long categoryId){
         try {
             String status = categoryService.deleteCategory(categoryId);
@@ -40,7 +42,7 @@ public class CategoryController {
             return new ResponseEntity<>(e.getReason(), e.getStatusCode());
         }
     }
-    @PutMapping("/api/public/categories/{categoryId}")
+    @PutMapping("/public/categories/{categoryId}")
     public ResponseEntity<String> updateCategory(@RequestBody Category category, @PathVariable Long categoryId){
         try {
             Category savedCategory = categoryService.updateCategory(category, categoryId);
