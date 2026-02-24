2d973f8d56f86dc5c4d37f3eb15261e5b44fe69c
sunnyraj <mrsj5714@gmail.com>
2026-02-24 19:01:15 +0530

    cleanup category code

--- sb-ecom/src/main/java/com/ecommerce/project/controller/CategoryController.java
+++ sb-ecom/src/main/java/com/ecommerce/project/controller/CategoryController.java
@@ -33,24 +33,14 @@ public class CategoryController {
     }
     @DeleteMapping("/admin/categories/{categoryId}")
     public ResponseEntity<String> deleteCategory(@PathVariable Long categoryId){
-        try {
             String status = categoryService.deleteCategory(categoryId);
             return new ResponseEntity<>(status, HttpStatus.OK);
 //          return ResponseEntity.ok(status);
 //          return ResponseEntity.status(HttpStatus.OK).body(status);
-        }
-        catch (ResponseStatusException e) {
-            return new ResponseEntity<>(e.getReason(), e.getStatusCode());
-        }
     }
     @PutMapping("/public/categories/{categoryId}")
-    public ResponseEntity<String> updateCategory(@RequestBody Category category, @PathVariable Long categoryId){
-        try {
+    public ResponseEntity<String> updateCategory(@Valid @RequestBody Category category, @PathVariable Long categoryId){
             Category savedCategory = categoryService.updateCategory(category, categoryId);
             return new ResponseEntity<>("Category updated with categoryId: "+categoryId, HttpStatus.OK);
-        }
-        catch (ResponseStatusException e) {
-            return new ResponseEntity<>(e.getReason(), e.getStatusCode());
-        }
     }
 }
