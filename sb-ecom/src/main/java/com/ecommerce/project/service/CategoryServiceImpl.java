0b9eb6289164026e8041263c5ef7633d11567fea
sunnyraj <mrsj5714@gmail.com>
2026-02-24 18:05:04 +0530

    add api exception file

--- sb-ecom/src/main/java/com/ecommerce/project/service/CategoryServiceImpl.java
+++ sb-ecom/src/main/java/com/ecommerce/project/service/CategoryServiceImpl.java
@@ -72,7 +72,10 @@ public class CategoryServiceImpl implements CategoryService {
 
     @Override
     public List<Category> getAllCategories() {
-        return categoryRepository.findAll();
+        List<Category> categories = categoryRepository.findAll();
+        if(categories.isEmpty())
+            throw new APIException("No category created till now.");
+        return categories;
     }
 
     @Override
