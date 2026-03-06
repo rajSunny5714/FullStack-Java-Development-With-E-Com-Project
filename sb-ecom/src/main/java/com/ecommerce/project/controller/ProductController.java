eaf192c0f18c8df9ac50e12e65f84e98fa5b9cad
sunnyraj <mrsj5714@gmail.com>
2026-03-06 10:35:27 +0530

    function added update productImage

--- sb-ecom/src/main/java/com/ecommerce/project/controller/ProductController.java
+++ sb-ecom/src/main/java/com/ecommerce/project/controller/ProductController.java
@@ -9,6 +9,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 @RestController
 @RequestMapping("/api")
@@ -54,4 +57,11 @@ public class ProductController {
         return new ResponseEntity<>(deletedProduct, HttpStatus.OK);
     }
 
+    @PutMapping("products/{productId}/image")
+    public ResponseEntity<ProductDTO> updateProductImage(@PathVariable Long productId,
+                                                         @RequestParam("image") MultipartFile
+                                                                 image) throws IOException {
+        ProductDTO updatedProduct = productService.updateProductImage(productId, image);
+        return new ResponseEntity<>(updatedProduct, HttpStatus.OK);
+    }
 }
